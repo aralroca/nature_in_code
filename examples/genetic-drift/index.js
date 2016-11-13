cd3f073b4c40ab1e0f3b02b52bab6ea5ce095964
Aral Roca Gomez <aral-rg@hotmail.com>
2016-11-13 02:47:20 +0100

    Refactoring Genetic Drift example

--- examples/genetic-drift/index.js
+++ examples/genetic-drift/index.js
@@ -3,14 +3,15 @@ import { round } from './lib/utils.js';
 
 const N = 1000;
 const generations = 1000;
+const p = 0.5;
 const data = Array(generations).fill(1);
 
 const nextGeneration = n => p => Array(n * 2).fill(1)
-  .map(x => Math.random() < p)
+  .map(() => Math.random() < p)
   .filter(a1 => a1).length / (n * 2);
 
 const nextGenerationKIndividuals = nextGeneration(N);
 
-data.forEach((p, i, a) => a[i] = nextGenerationKIndividuals(a[i-1] || 0.5));
+data.forEach((x, i, a) => a[i] = nextGenerationKIndividuals(a[i-1] || p));
 
-drawLineChart(data,"Generation","p",["Population Size:",N,"Generations:",generations]);
+drawLineChart(data,'Generation','p',['Population Size:',N,'Generations:',generations]);
