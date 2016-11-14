ae8f5e6c749c4314a5910f1012ba22b264a2c319
Aral Roca Gomez <aral-rg@hotmail.com>
2016-11-14 01:34:22 +0100

    Update Genetic drift notes and example

--- examples/genetic-drift/index.js
+++ examples/genetic-drift/index.js
@@ -1,17 +1,19 @@
 import { drawLineChart } from './lib/visualize.js';
-import { round } from './lib/utils.js';
 
-const N = 1000;
-const generations = 1000;
+const N = 200;
+const generations = 2000;
 const p = 0.5;
-const data = Array(generations).fill(1);
 
 const nextGeneration = n => p => Array(n * 2).fill(1)
   .map(() => Math.random() < p)
   .filter(a1 => a1).length / (n * 2);
 
-const nextGenerationKIndividuals = nextGeneration(N);
+const getSimulation = () => {
+  const nextGenerationKIndividuals = nextGeneration(N);
+  const simulation = Array(generations).fill(1);
 
-data.forEach((x, i, a) => a[i] = nextGenerationKIndividuals(a[i-1] || p));
+  simulation.forEach((x, i, a) => a[i] = nextGenerationKIndividuals(a[i-1] || p));
+  return simulation;
+};
 
-drawLineChart(data,'Generation','p',['Population Size:',N,'Generations:',generations]);
+drawLineChart(Array(10).fill(1).map(getSimulation),'Generation','p',['Population Size:',N,'Generations:',generations]);
