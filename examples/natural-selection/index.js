08e77e79ce66824bd728649b2438806b8a758382
Aral Roca Gomez <aral-rg@hotmail.com>
2016-12-02 18:58:01 +0100

    Update Natural Selection

--- examples/natural-selection/index.js
+++ examples/natural-selection/index.js
@@ -1,31 +1,46 @@
 import { drawLineChart } from './lib/visualize.js';
 
-const s = 0.1;
-const h = 1.5; // Change 0.2 to -1.5
-const generations = 400;
-const data = [];
+const generations = 4000; // Increment *10 generations
+const sh = 0.2;
+const sp = 0.5; 
 
-const calculateDeltaP = (s, h, p, q) => 
-  (p * q * s * (p * h + q * (1 - h))) / (1 - 2* p * q * h * s - 2 * q * s);
-  
-// Add pValues an array of 99 0.01 to 1 (1 not included)  
-const pValues = Array(99).fill(1);
+/**
+ * Selection (Host or Parasite)
+ */
+function selection(firstFreq, secondFreq, isHost = false) {
+  const sum = (a, b) => a + b;
+  const hostFit = i => (p, ii) => (i === ii ? 1-sh : 1) * p;
+	const parasitFit = i => (h, ii) => (i === ii ? 1 : 1-sp) * h;
+  const calcFit = i => (x, ii) => isHost? hostFit(i)(x, ii): parasitFit(i)(x, ii);
+  const noise = Math.random() * 0.01; // Add NOISE
 
-// Modify p values vs time
-pValues.forEach((x, i, a) => (a[i] = (a[i-1] || 0) + 0.01));
+	const newFrequencies = firstFreq.map((x, i) =>
+       x * secondFreq.map(calcFit(i)).reduce(sum, 0)+ noise); //Add NOISE
 
-const runSimulation = p => {
-  const results = Array(400).fill(1)
-    .map(x => {
-      const deltaP = calculateDeltaP(s, h, p, 1 - p);
-      p += deltaP;
-      return p;
-    });
-    data.push(results);
-};
+  const sumFrequencies = newFrequencies.reduce(sum, 0);
 
-// Run simulations with p as a parameter
-pValues.forEach(runSimulation);
+	return newFrequencies.map(x => x / sumFrequencies);
+}
 
-drawLineChart(data, 'Generation', 'p', []);
+/**
+ *  Visualization
+ */
+function runVisualization() {
+  const tmpData = Array(generations).fill(1);
+  const generateFreq = a1Freq => [a1Freq, 1 - a1Freq];
+  let data = [[], [], [], []]; // Initialize to array with 4 arrays
+  let hosts = generateFreq(Math.random());
+  let parasites = generateFreq(Math.random());
 
+  data.forEach((x, i, a) => a[i].push(i > 2 ? hosts[i%2] : parasites[i%2]));
+
+  for(let i = 0; i < generations; i += 1) {
+      hosts = selection(hosts, parasites, true);
+      parasites = selection(parasites, hosts);
+      data.forEach((x, i, a) => a[i].push(i > 2 ? hosts[i%2] : parasites[i%2]));
+  }
+
+  drawLineChart(data, "Generation", "p", []);
+}
+
+runVisualization();
